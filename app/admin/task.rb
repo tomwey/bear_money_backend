29919efb401af1d4e4cc42e65bd651bd57ee886b
tomwey <tomwey@163.com>
2018-07-30 13:24:36 +0800

    add some func

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -13,4 +13,22 @@ permit_params :title, :body, :money, :task_type_id, :total_count, :expired_at, :
 #   permitted
 # end
 
+form do |f|
+  f.semantic_errors
+  f.inputs '基本信息' do
+    f.input :task_type_id, as: :select, label: '所属分类', collection: TaskType.where(opened: true).map { |t| [t.name, t.uniq_id] }, required: true
+    f.input :title
+    f.input :body, as: :text, input_html: { class: 'redactor' }, placeholder: '网页内容，支持图文混排', hint: '网页内容，支持图文混排'
+    f.input :_money, as: :number, label: '任务报酬', placeholder: "单位为：元，例如：1.5"
+    f.input :total_count, placeholder: '必须输入一个大于0的整数'
+    f.input :expired_at, as: :string, placeholder: '2018-10-32 23:59'
+    f.input :repeat_count
+    f.input :approve_duration, placeholder: '例如：24小时'
+    f.input :avg_approve, placeholder: '例如：15小时'
+    f.input :sort, placeholder: '值越大显示越靠前'
+    f.input :opened
+  end
+  actions
+end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  validates :title, :body, :money, :task_type_id, :total_count, :expired_at, presence: true
+  validates :title, :body, :money, :_money, :task_type_id, :total_count, :expired_at, presence: true
   before_create :generate_uniq_id
   def generate_uniq_id
     begin
@@ -9,5 +9,17 @@ class Task < ActiveRecord::Base
       end
       self.uniq_id = (n.to_s + SecureRandom.random_number.to_s[2..8]).to_i
     end while self.class.exists?(:uniq_id => uniq_id)
+  end
+  
+  def _money=(val)
+    if val.present?
+      self.money = (val.to_f * 100.0).to_i
+    end
+  end
+  
+  def _money
+    if self.money
+      (self.money / 100.0)
+    end
   end
 end
